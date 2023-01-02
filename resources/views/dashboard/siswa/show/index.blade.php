802889a73232b21217a85b0d792020edfa8e9ae7
farrel07 <farrqeel@gmail.com>
2023-01-02 13:18:59 +0700

    jaga jaga

--- resources/views/dashboard/siswa/show/index.blade.php
+++ resources/views/dashboard/siswa/show/index.blade.php
@@ -59,7 +59,7 @@
     <section class="text-center">
         @if (!$user->img)
             <a class="href-pisan" href="" data-bs-toggle="modal" data-bs-target="#modalAdd">
-                <img src="{{ asset('storage/default.jpg') }}" width="200" class="imgPisan img-fluid rounded-circle shadow mb-2">
+                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width="200" class="imgPisan img-fluid rounded-circle shadow mb-2">
                 <div class="middle">
                     <p class="text">Change Picture</p>
                 </div>

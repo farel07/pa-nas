a20b26a5c3af9b56e9087b398c42d7877c780ba6
farrel07 <farrqeel@gmail.com>
2023-02-01 08:41:19 +0700

    sedikit otak atik view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,20 +35,13 @@
 				<span class="login100-form-title p-b-41">
 					Account Login
 				</span>
-				@error('username')
-					<div class="alert alert-danger alert-dismissible fade show" role="alert">
-						{{ $message }}
-						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					</div>
-				@enderror
 				<form action="/login" method="POST" class="login100-form validate-form p-b-33 p-t-5">
 					@csrf
 
-					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="Username">
+					<div class="wrap-input100 validate-input @error('username') border-danger @enderror" data-validate = "Enter username">
+						<input class="input100" type="text" name="username" placeholder="@error('username') Enter Valid @enderror Username">
 						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
 					</div>
-
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
 						<input class="input100" type="password" name="password" placeholder="Password">
 						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
